003fc56b4e7e8aa687f087878d3e18ee11d6a597
Ivan Kovalenko <raintimeunhandledexcepti0n@gmail.com>
2025-03-27 19:32:02 +0100

    FIX: correct limits, correct drawing, better prompt

--- main.c
+++ main.c
@@ -4,15 +4,15 @@
 #define HEIGHT      10
 #define WIDTH       30
 
-static int sx, sy, fx, fy;
+static int sx = WIDTH/2;
+static int sy = HEIGHT/2;
+static int fx, fy;
 static int gameover = 0;
+
 static unsigned int score = 0;
 
 static void gen_fruit(void)
 {
-    sx = WIDTH/2;
-    sy = HEIGHT/2;
-
     fx = arc4random_uniform(WIDTH);
     fy = arc4random_uniform(HEIGHT);
 
@@ -26,42 +26,6 @@ static void gen_fruit(void)
 
 static void draw(void)
 {
-    char c_to_print;
-
-    // up
-    for (int i = 0; i < WIDTH; ++i)
-        printf("*");
-    printf("\n");
-
-    for (int i = 0; i < HEIGHT; ++i) {
-        for (int j = 0; j <= WIDTH; ++j) {
-
-            c_to_print = ' ';
-            // corners
-            if (j == 0 || j == WIDTH-1) {
-                printf("+");
-            }
-
-            if (j == fx && i == fy) {
-                printf("@");
-                j++; i++;
-            }
-
-            if (j == sx && i == sy) {
-                printf("0");
-                j++; i++;
-            }
-
-            printf(" ");
-        }
-        printf("\n");
-    }
-
-    // down
-    for (int i = 0; i < WIDTH; ++i)
-        printf("*");
-    printf("\n");
-
     printf(" --------------------------------\n");
     printf("|           (w -> up)            |\n");
     printf("|               ^                |\n");
@@ -71,9 +35,44 @@ static void draw(void)
     printf("|               v                |\n");
     printf("|           (s -> down)          |\n");
     printf(" --------------------------------\n");
-    printf("sy: %d\n", sy);
-    printf("sx: %d\n", sx);
-    printf("score: %d\n", score);
+    printf("[sy: %d]\n", sy);
+    printf("[sx: %d]\n", sx);
+
+    printf("~~~~~~~~~\n");
+    printf("SCORE: %d\n", score);
+    printf("~~~~~~~~~\n");
+
+    // up
+    for (int i = 0; i <= WIDTH; ++i)
+        printf("*");
+    printf("\n");
+
+    for (int i = 0; i < HEIGHT; ++i) {
+        for (int j = 0; j <= WIDTH; ++j) {
+
+            // corners
+            if (j == 0 || j == WIDTH-1) {
+                printf("+");
+            }
+
+            if ((j == fx && i == fy) && (j == sx && i == sy)) {
+                printf("0");
+            } else if (j == fx && i == fy) {
+                printf("@");
+            } else if (j == sx && i == sy) {
+                printf("0");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    // down
+    for (int i = 0; i <= WIDTH; ++i)
+        printf("*");
+    printf("\n");
+
 }
 
 static void input(void)
@@ -100,7 +99,7 @@ static void logic(void)
         gen_fruit();
     }
 
-    if (sx <= 1 || sx >= WIDTH-1 || sy <= 1 || sy >= HEIGHT-1) gameover = 1;
+    if (sx < 1 || sx >= WIDTH-1 || sy < 0 || sy >= HEIGHT) gameover = 1;
 }
 
 int main(void)
